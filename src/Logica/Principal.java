0e1ee679f7c4521709897f4240e0bc6fb9b4db8e
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-06-15 22:31:15 -0300

    Creando el repositorio

---
+++ src/Logica/Principal.java
@@ -0,0 +1,13 @@
+package Logica;
+import Igu.Formulario;
+
+
+public class Principal {
+  
+    public static void main(String[] args) {
+        Formulario pantalla = new Formulario(); //creo el formulario
+        pantalla.setVisible(true); //la hace visible
+        pantalla.setLocationRelativeTo(null); //la centra en pantalla
+    }
+    
+}
